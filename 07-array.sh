71d48061648b7b55d1709b349422a3ee5db3391f
rajeshmad <techaddictray@gmail.com>
2025-07-15 23:29:25 +0530

    07.array.sh update

---
+++ 07-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("MISSION IMPOSSIBLE" "ICE AGE" "DARK KINGHT")
+
+echo "First movi: $MOVIES[0]"
+echo "Second movi: $MOVIES[1]"
+echo "Third movi: $MOVIES[2]"
+echo "Fourth movi:$MOVIES[3]"
+
+echo "All movies:$MOVIES[@]"
