826990f5020e8f481757d271894bdf4af3ed34aa
rajeshmad <techaddictray@gmail.com>
2025-07-15 12:13:06 +0530

    hello world started

---
+++ 03-varaiables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person 1 = Trump
+person 2 = Musk
+echo "person 1 : Hi Musk, How are you doing?"
+echo "person 2 : I'm doing fine. How are you?"
+echo "person 1 : When are you giing to Mars?"
+echo "person 2 : May be tommarow"
