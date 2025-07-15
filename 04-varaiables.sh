e29de1d3985e0811b674f4d75acd3f110fc37cae
rajeshmad <techaddictray@gmail.com>
2025-07-15 21:40:44 +0530

    hello world started

---
+++ 04-varaiables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+echo "$person1 : Hi Musk, How are you doing?"
+echo "$person2 : I'm doing fine. How are you?"
+echo "$person1 : When are you giing to Mars?"
+echo "$person2 : May be tommarow"
