89fc2d50c836c7a5904847d156960cc01d6ff77d
rajeshmad <techaddictray@gmail.com>
2025-07-16 00:04:15 +0530

    08.special-vars.sh update

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "All variables passed to the script: $@"
 echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Home directory of the User: $HOME"
+echo "User running this script: $USER"
+echo "PID of the Script: $$"
+sleep 10 &
+echo "PID of the last command:$!"
