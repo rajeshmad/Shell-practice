99b2bc8ed2bc557976f2a97b4c664622f89fcee9
rajeshmad <techaddictray@gmail.com>
2025-07-23 10:38:19 +0530

    update practice notes

--- 00-Practice.sh
+++ 00-Practice.sh
@@ -13,3 +13,24 @@ then
 else
     echo "Running with admin access"
 fi
+
+#2. check if MySQl is already installed or not
+
+dnf installed list mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySql is not installed going to install.."
+
+dnf install mysql -y
+
+if [ $? -equal 0 ]
+then 
+    echo "MySql is installation is ..successfull"
+else
+    echo "MySQl intsallation is failur"
+fi
+
+else
+    echo "MySql is already installed"
+    
